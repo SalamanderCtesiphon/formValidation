3e1df72db28cd0ca21defdc8b308433f27b24fa1
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-21 11:39:56 -0600

    created the regex experssions for the javascript form validation test

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const uname = document.getElementById('uname');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
+const form = document.getElementById('form');
+
+const valUserName = /[a-z]\.[a-z]{3,}/;
+const valEmail = /[a-zA-Z0-9]{1,}\@[bigcorp.eu]/;
+const valPhone = /[0-9]{3}-[0-9]{3}-[0-9]{4}|[0-9]{10}|[0-9]{3}\.[0-9]{3}\s[0-9]{4}|[0-9]{3}\s[0-9]{3}\s[0-9]{4}/;
+
+email.addEventListener('input', (event) => {
